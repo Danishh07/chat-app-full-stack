7376b1c844d0b59258d2f99ebbbf76fcd96496ab
Danish Alam <danishalam050@gmail.com>
2025-05-12 00:02:06 +0530

    bug fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chat-app-full-stack-0pf9.onrender.com/api",
   withCredentials: true,
 });
+
